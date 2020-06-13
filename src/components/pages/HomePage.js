235732d7a5703e908fa4389c0abe5d1ed105c063
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-06-13 15:39:29 -0700

    Added button & dropdowns to homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,29 +3,140 @@ import React from "react";
 export default function HomePage() {
   return (
     // <!-- Example single danger button -->
-    <div className="btn-group">
-      <button
-        type="button"
-        className="btn btn-danger dropdown-toggle"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-      >
-        Gender
-      </button>
-      <div className="dropdown-menu">
-        <button className="dropdown-item" href="#">
-          Action
-        </button>
-        <button className="dropdown-item" href="#">
-          Another action
-        </button>
-        <button className="dropdown-item" href="#">
-          Something else here
-        </button>
-        <div className="dropdown-divider"></div>
-        <button className="dropdown-item" href="#">
-          Separated link
-        </button>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-6 offset-3">
+          <h1 className="text-center ">Gift Generator</h1>
+        </div>
+        <div className="float-left">
+          <button type="button" class="btn btn-success">
+            Success
+          </button>
+        </div>
+      </div>
+      {/* Seperate row for titles of each select menu TODO: clean up code  */}
+      <div className="row mt-3">
+        <div className="col-3 text-center">
+          <h5>Gender</h5>
+        </div>
+        <div className="col-3 text-center">
+          <h5>Age Group</h5>
+        </div>
+        <div className="col-3 text-center">
+          <h5>Interest</h5>
+        </div>
+        <div className="col-3 text-center">
+          <h5>Price Range</h5>
+        </div>
+      </div>
+      {/* TODO: add functionality */}
+      <div className="row">
+        <div className="col-3">
+          <select className="custom-select bg-primary text-white">
+            <option value="0">Nothing Selected</option>
+            <option value="1">Male</option>
+            <option value="2">Female</option>
+            <option value="3">Gender Neutral</option>
+          </select>
+        </div>
+        <div className="col-3">
+          <select className="custom-select">
+            <option selected>Nothing Selected</option>
+            <option value="1">Under 12</option>
+            <option value="2">12-17</option>
+            <option value="3">18-24</option>
+            <option value="4">25-34</option>
+            <option value="5">35-44</option>
+            <option value="6">45-54</option>
+            <option value="7">55+</option>
+          </select>
+        </div>
+        {/* Allow multiple selects */}
+        <div className="col-3">
+          <select className="custom-select">
+            <option selected>Nothing Selected</option>
+            <option value="1">Arts and Crafts</option>
+            <option value="2">Collectibles</option>
+            <option value="3">Electronics</option>
+            <option value="4">Jewelry</option>
+            <option value="5">Toys</option>
+            <option value="6">Sports & Recreation</option>
+            <option value="7">Outdoors</option>
+            <option value="8">Music</option>
+            <option value="9">Fashion</option>
+            <option value="10">Games</option>
+          </select>
+        </div>
+        {/* Maybe add dragbar to adjust range */}
+        <div className="col-3">
+          <select className="custom-select">
+            <option selected>Nothing Selected</option>
+            <option value="1">Under $25</option>
+            <option value="2">$25-$50</option>
+            <option value="3">$50-$75</option>
+            <option value="4">$75-$100</option>
+            <option value="5">$100-$200</option>
+            <option value="5">$200+</option>
+          </select>
+        </div>
+      </div>
+      <h2 className="text-center my-5">Most Popular Gifts</h2>
+      <div className="offset-4 col-6">
+        <div>
+          {/* Placeholders for now, come back to add components */}
+          <div className="row mb-2">
+            <div className="containsImage fitToImageContainer">
+              <div className="col-5">
+                <img alt="item" />
+              </div>
+            </div>
+            <div className="col-5 ml-5">
+              <text>Name: </text>
+              <br />
+              <br />
+              <text>Description: </text>
+              <br />
+              <br />
+              <text>Price: </text>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="row mb-2">
+            <div className="containsImage fitToImageContainer">
+              <div className="col-5">
+                <img alt="item" />
+              </div>
+            </div>
+            <div className="col-5 ml-5">
+              <text>Name: </text>
+              <br />
+              <br />
+              <text>Description: </text>
+              <br />
+              <br />
+              <text>Price: </text>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="row mb-2">
+            <div className="containsImage fitToImageContainer">
+              <div className="col-5">
+                <img alt="item" />
+              </div>
+            </div>
+            <div className="col-5 ml-5">
+              <text>Name: </text>
+              <br />
+              <br />
+              <text>Description: </text>
+              <br />
+              <br />
+              <text>Price: </text>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
